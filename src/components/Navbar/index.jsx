f01eef44b5613ec43d1a4bd9d0a55c2ac85d160e
Caro407 <caro.lefevre407@hotmail.fr>
2021-05-18 16:44:10 +0200

    set up tailwind

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
 
   return (
-    <h2>Je suis une navbar</h2>
+    <nav className="h-10 w-full border-b border-1 my-2">
+      <div>
+        <Link to="/home" className="mx-2">Accueil</Link>
+        <Link to="/sign-up" className="mx-2">Sign Up</Link>
+      </div>
+    </nav>
   )
 }
 
