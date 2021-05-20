13189cee6e3dc333b8121e563c4c5864400ec052
Caro407 <caro.lefevre407@hotmail.fr>
2021-05-20 14:58:17 +0200

    posts can be deleted

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,12 +1,29 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import LikeButton from './../LikeButton'
+import LikeButton from './../LikeButton';
+import { useSelector } from 'react-redux';
+import Cookies from 'js-cookie';
 
 const Card = (props) => {
+  const [isAuthor, setAuthor] = React.useState(false)
+  const user = useSelector(state => state.user);
+
+  const isUserAuthor = () => {
+    if(props.data.user.id === user.id) {
+      setAuthor(true)
+    };
+  };
+
+  const deletePostById = () => {
+    props.deletePost(props.data.id)
+  };
+
+  React.useEffect(() => {isUserAuthor()}, []);
 
   return (
     <div className='post-container'>
       <h3>{props.data.text}</h3>
+      {isAuthor ? <button onClick={deletePostById}>Delete post</button> : <p></p>}
       <LikeButton likes={props.data.like} id={props.data.id} />
       <Link to={`/users/${props.data.user.id}`}>- {props.data.user.username}</Link>
     </div>
--- src/pages/Posts/index.jsx
+++ src/pages/Posts/index.jsx
@@ -19,7 +19,22 @@ const Posts = () => {
         console.log(response);
         setPosts(response)
       })
-  }
+  };
+
+  const deletePost = (id) => {
+    fetch(`http://localhost:1337/posts/${id}`, {
+      method: 'delete',
+      headers: {
+        'Authorization': `Bearer ${Cookies.get('token')}`,
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(response => response.json())
+      .then(response => {
+        console.log(response);
+        fetchPosts();
+      })
+  };
 
   React.useEffect(() => {fetchPosts()}, []);
 
@@ -28,7 +43,7 @@ const Posts = () => {
       <h2>Posts</h2>
       <div className="grid grid-cols-3 gap-4">
         {posts.map((post, index) =>
-          <Card data={post} key={index}/>
+          <Card data={post} key={index} deletePost={deletePost}/>
         )}
       </div>
       <Link to="/create-post">Create new post</Link>
